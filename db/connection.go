c988f18eb1874b8b4dce9ebebe3d30ac07f34d43
riansyah <mriansyah93@gmail.com>
2022-07-05 08:34:07 +0700

    init connection of redis

--- db/connection.go
+++ db/connection.go
@@ -1,6 +1,11 @@
 package db
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+
 	"github.com/go-redis/redis/v9"
 )
 
@@ -8,6 +13,21 @@ type RedisClient struct {
 	redisCl *redis.Client
 }
 
-func GetRedisClient() {
-
+func GetRedisClient() *RedisClient {
+	var once sync.Once
+	var rcl *RedisClient
+	once.Do(func() {
+		client := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "",
+			DB:       0,
+		})
+		rcl = &RedisClient{client}
+	})
+	_, err := rcl.redisCl.Ping(context.TODO()).Result()
+	if err != nil {
+		fmt.Println("running redis-server --daemonize yes")
+		log.Fatalf("Could not connect to redis %v", err)
+	}
+	return rcl
 }
