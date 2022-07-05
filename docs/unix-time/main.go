03092e42247cfa29c57a4247e9f31b02d7359a2b
riansyah <mriansyah93@gmail.com>
2022-07-05 10:31:59 +0700

    add unix time, ignore redis_test

---
+++ docs/unix-time/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Defining t in UTC for Unix method
+
+	// year int, month Month, day, hour, min, sec, nsec int
+	t := time.Date(2020, 11, 14, 11, 30, 32, 0, time.UTC)
+
+	// Calling Unix method
+	unix := t.Unix()
+
+	// Prints output
+	fmt.Printf("%v\n", unix)
+}
