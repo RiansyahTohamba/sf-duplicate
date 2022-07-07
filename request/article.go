988bed87c9579bc030e4cc0038efa3dc5c00bc9f
riansyah <mriansyah93@gmail.com>
2022-07-07 15:13:14 +0700

    remove unnecessary code

--- request/article.go
+++ request/article.go
@@ -1,16 +1,9 @@
 package request
 
 type ArticleRequest struct {
-	// sebagai hash key
 	Poster string
 	Title  string
 	Link   string
 	Time   int64
 	Votes  float64
 }
-
-// title: "How to specify go-redis expires"
-// link: https://stackoverflow.com
-// poster: user:832
-// time: 1331344699.33
-// votes: 528
