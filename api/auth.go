bbdfd6081e4616e951b6fa0ee8ca9bd17f0bae22
riansyah <mriansyah93@gmail.com>
2022-07-12 10:04:23 +0700

    add session handler for login and logout function

--- api/auth.go
+++ api/auth.go
@@ -1,13 +1,31 @@
 package api
 
 import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func Login(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+	// id & email didapat darimana?
+	id := 122323
+	email := "mriansyah93@gmail.com"
 
+	session.Set("id", id)
+	session.Set("email", email)
+	session.Save()
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "User Sign In successfully",
+	})
 }
 
 func Logout(ctx *gin.Context) {
-
+	session := sessions.Default(ctx)
+	session.Clear()
+	session.Save()
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "success logout",
+	})
 }
