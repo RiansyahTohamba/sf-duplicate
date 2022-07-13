cbd2803c35031ecfa60c47247ee350137f6edaa3
riansyah <mriansyah93@gmail.com>
2022-07-13 13:54:59 +0700

    handle loginrequest from rest api

--- api/handler/user.go
+++ api/handler/user.go
@@ -1,43 +1,75 @@
 package handler
 
 import (
-	"crypto/rand"
-	"encoding/hex"
+	"errors"
+	"fmt"
 	"net/http"
+	"sf-duplicate/repository"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
-func Login(ctx *gin.Context) {
-	session := sessions.Default(ctx)
-	// token & email didapat darimana?
-
-	email := "mriansyah93@gmail.com"
-
-	// userid akan dipakai untuk mencatat recently view
-	// userId :=
-	session.Set("userId", userId)
-	session.Save()
-
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "User Sign In successfully",
-	})
+type UserHandler struct {
+	usrRepo *repository.UserRepository
 }
 
-func Logout(ctx *gin.Context) {
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+// TODO: extend later
+type RegisterRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func NewUserHandler(usrRepo *repository.UserRepository) *UserHandler {
+	return &UserHandler{usrRepo}
+}
+
+func (usr *UserHandler) Login(ctx *gin.Context) {
+	var loginReq LoginRequest
+	var verr validator.ValidationErrors
+
+	err := ctx.BindJSON(&loginReq)
+
+	if err != nil {
+		if errors.As(err, &verr) {
+			ctx.AbortWithStatusJSON(
+				http.StatusBadRequest,
+				gin.H{"errors": verr},
+			)
+		}
+	}
+
+	user, err := usr.usrRepo.Login(loginReq.Email, loginReq.Password)
+
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"message": err.Error(),
+		})
+	} else {
+		session := sessions.Default(ctx)
+		userId := fmt.Sprintf("user:%d", user.ID)
+		session.Set("user_id", userId)
+		session.Save()
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "User Sign-in successfully",
+		})
+
+	}
+
+}
+
+func (usr *UserHandler) Logout(ctx *gin.Context) {
 	session := sessions.Default(ctx)
 	session.Clear()
 	session.Save()
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": "success logout",
 	})
-}
-
-func generateSecureToken(length int) string {
-	b := make([]byte, length)
-	if _, err := rand.Read(b); err != nil {
-		return ""
-	}
-	return hex.EncodeToString(b)
 }
